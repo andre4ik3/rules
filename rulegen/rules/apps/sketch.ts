eda3dbf2d438a3a0870e8c5779beedd55747737e
andre4ik3 <62390580+andre4ik3@users.noreply.github.com>
2023-08-30 14:07:26 +0400

    Specify https for sketch rules, remove duplicates.

--- rulegen/rules/apps/sketch.ts
+++ rulegen/rules/apps/sketch.ts
@@ -18,29 +18,21 @@ export const rules: Rule[] = [
       "resources-live.sketch.cloud",
       "www.sketch.com",
     ]],
+    using: [[Protocol.TCP, 443]],
     notes: "Allows Sketch to log into accounts and validate license keys.",
   }),
 
   makeRule({
     process: [Paths.sketch],
     remote: [RemoteType.Host, ["download.sketch.com"]],
+    using: [[Protocol.TCP, 443]],
     notes: "Allows Sketch to download and check for updates.",
   }),
 
   makeRule({
     process: [Paths.sketch],
-    remote: [RemoteType.Host, [
-      "graphql.sketch.cloud",
-      "api.prod.sketch.com",
-      "resources-live.sketch.cloud",
-      "www.sketch.com",
-    ]],
-    notes: "Allows Sketch to log into accounts and validate license keys.",
-  }),
-
-  makeRule({
-    process: [Paths.sketch],
     remote: [RemoteType.Host, ["developer.apple.com", "devimages-cdn.apple.com"]],
+    using: [[Protocol.TCP, 443]],
     notes: "Allows Sketch to download Apple's iOS design library.",
   }),
 ].flat();
